ca157a98b59bbb3dfd54fdd07731d6ea40750bd3
AngelaFang <1823476439@qq.com>
2020-04-30 10:10:35 +0800

    deploy

---
+++ precache-manifest.9ca3dc77c7c6452673b2308c5b327dfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b9d0cc958fc6abe455",
+    "url": "/jijianjizhang-view/css/app.347e516f.css"
+  },
+  {
+    "revision": "9c00557e0644e9f67720c8fbddae44eb",
+    "url": "/jijianjizhang-view/img/logo.9c00557e.png"
+  },
+  {
+    "revision": "e9be9ec8113de025abf43c7c0921559e",
+    "url": "/jijianjizhang-view/index.html"
+  },
+  {
+    "revision": "62b9d0cc958fc6abe455",
+    "url": "/jijianjizhang-view/js/app.d3c3cb73.js"
+  },
+  {
+    "revision": "d233ef3c9b3c2c8263f4",
+    "url": "/jijianjizhang-view/js/chunk-vendors.ffb21e19.js"
+  },
+  {
+    "revision": "6d910e903a9d2e358b96632712056a74",
+    "url": "/jijianjizhang-view/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jijianjizhang-view/robots.txt"
+  }
+]);
